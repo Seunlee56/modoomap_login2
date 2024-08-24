3b8737e7d4b216438d7c21e57ad320051a4f2023
seunlee <rarafamily@naver.com>
2024-08-25 01:07:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/modoomap_login2/SignUpActivity.kt
@@ -0,0 +1,25 @@
+package com.example.modoomap_login2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.modoomap_login2.databinding.ActivityMainBinding
+import com.example.modoomap_login2.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
